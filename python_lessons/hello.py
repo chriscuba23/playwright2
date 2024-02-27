8d23d441bd6ef3c3fb5e87e98b227e8a98d16154
Chris Kouvaras <chriscuba23@gmail.com>
2024-02-27 09:11:17 +0200

    hi

---
+++ python_lessons/hello.py
@@ -0,0 +1,9 @@
+def main(string):
+    print('Hello World! ' + string)
+
+def secondary_function():
+    print('Hello again!')
+ 
+
+main('you')
+secondary_function()
---
+++ python_lessons/print.py
@@ -0,0 +1,12 @@
+def square(num):
+  numSquared = num * num
+  return numSquared
+
+# several test cases
+# to verify the function
+# is working as expected
+
+print(square(2)) # Prints 4
+print(square(5)) # Prints 25  
+print(square(8)) # Prints 64
+print(square(10)) # Prints 100
